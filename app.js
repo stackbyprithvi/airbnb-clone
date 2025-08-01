99e6f92a214600f2c02761a17f2d5a9649fb8872
PRITHVI RAJ PANDEY <pandeyprithvi69@gmail.com>
2025-08-01 21:33:00 +0545

    WORKING ON CRUD OPERATIONS

--- app.js
+++ app.js
@@ -2,35 +2,47 @@ const express=require('express')
 const app =express();
 const mongoose=require('mongoose');
 const Listing=require('./models/listing');
+const path=require('path');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended:true}));
 
 main()
 .then(() =>{
     console.log("Connected to MongoDB")
 })
 .catch((err) => console.log(err));  
-    
-
 async  function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/Airbnb-Clone')
 }
-
 app.get('/',(req,res)=>{
     res.send("WELCOME TO AIRBNB CLONE");
 })
-// app.get('/testListing',async (req,res)=>{
-//     let sampleListing = new Listing({
-//         title: "Cozy Beachfront Cottage",
-//     description:
-//       "Escape to this charming beachfront cottage for a relaxing getaway. Enjoy stunning ocean views and easy access to the beach.",
-//     image: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-//     price: 1500,
-//     location: "Malibu",
-//     country: "United States",
-//   });
 
-  await sampleListing.save();
-  res.send("Sample listing created");
-});
+//SHOW LISTINGS ROUTE
+app.get('/listings',async (req,res)=>{
+  const allListings=  await Listing.find({});
+  res.render('listings/index.ejs',{allListings});
+    })
+
+    //CREATE LISTING ROUTE
+app.get('/listings/new',(req,res)=>{
+    res.render('listings/new.ejs');
+})
+
+    //SHOW ROUTE
+app.get('/listings/:id',async (req,res)=>{
+    const {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render('listings/show.ejs',{listing});
+})
+
+app.post('/listings',async (req,res)=>{
+const newListing= new Listing(req.body.listing);
+await newListing.save();
+res.redirect('/listings');
+})
+
 
 const PORT=3000;
 app.listen(PORT,()=>{
