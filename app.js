32f1b0e4290e0f19e6fc0d78580d2fa55073f797
PRITHVI RAJ PANDEY <pandeyprithvi69@gmail.com>
2025-08-01 18:59:34 +0545

    feat: add complete initial folder structure and files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const app =express();
 const mongoose=require('mongoose');
+const Listing=require('./models/listing');
 
 main()
 .then(() =>{
@@ -16,6 +17,20 @@ async  function main(){
 app.get('/',(req,res)=>{
     res.send("WELCOME TO AIRBNB CLONE");
 })
+// app.get('/testListing',async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title: "Cozy Beachfront Cottage",
+//     description:
+//       "Escape to this charming beachfront cottage for a relaxing getaway. Enjoy stunning ocean views and easy access to the beach.",
+//     image: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+//     price: 1500,
+//     location: "Malibu",
+//     country: "United States",
+//   });
+
+  await sampleListing.save();
+  res.send("Sample listing created");
+});
 
 const PORT=3000;
 app.listen(PORT,()=>{
